563751de9c42b73711a42fd424ce5fe0374f0e90
nouman_bhatti <nouman.bhatti@attendanywhere.com>
2022-09-28 13:06:26 +1000

    static page

---
+++ src/Component/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from '../../src/react-icon-small.png';
+function Navbar() {
+    return (
+        <nav className="nav">
+            <img src={logo} alt='React logo' className="nav--icon"  />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
+export default Navbar
